161f4f434ab677aab855415b0e5a530701b387c8
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-12-17 13:45:34 -0600

    drafting instance variables as attributes of pieces, color of piece and name

--- lib/chess_objects.rb
+++ lib/chess_objects.rb
@@ -2,28 +2,72 @@ class Chess_board
 	attr_accessor :board
 
 	def initialize
-	  # @board = Array.new(8) { Array.new(8) }
-      @board = [
-        ['R','k','B','Q','K','B','k','R'],
-        ['P','P','P','P','P','P','P','P'],
-        ['-','-','-','-','-','-','-','-'],
-        ['-','-','-','-','-','-','-','-'],
-        ['-','-','-','-','-','-','-','-'],
-        ['-','-','-','-','-','-','-','-'],
-        ['P','P','P','P','P','P','P','P'],
-        ['R','k','B','Q','K','B','k','R']
-      ]
+	   @board = Array.new(8) { Array.new(8) }
+      # @board = [
+      #   ['R','k','B','Q','K','B','k','R'],
+      #   ['P','P','P','P','P','P','P','P'],
+      #   ['-','-','-','-','-','-','-','-'],
+      #   ['-','-','-','-','-','-','-','-'],
+      #   ['-','-','-','-','-','-','-','-'],
+      #   ['-','-','-','-','-','-','-','-'],
+      #   ['P','P','P','P','P','P','P','P'],
+      #   ['R','k','B','Q','K','B','k','R']
+      # ]
 
 	end	
 
 	def print_board
         i = 0
         while i < board.length
-        	p board[i].join("   ")
+        	p board[i].join(" ? ")
         	i += 1
-        end	
-       
+        end	  
 	end	
 end	
 
- 
+class Pawn 
+  attr_accessor :position, :name, :color
+
+  def initialize
+    @name = 'Pawn'
+    @color = nil
+    # @position = [nil,nil]
+  end  
+end 
+
+
+class Rook
+  def initialize
+    @name = 'Rook'
+    @color = nil
+    # @position = [nil,nil] 
+  end  
+end 
+
+class Knight
+  def initialize 
+    @name = 'Knight'
+    @color = nil
+  end  
+end 
+
+class Bishop
+  def initialize 
+    @name = 'Bishop'
+    @color = nil
+  end  
+end 
+
+class Queen
+  def initialize
+    @name = 'Queen'
+    @color = nil 
+  end  
+end 
+
+class King
+  def initialize
+    @name = 'King'
+    @color = nil 
+  end  
+end 
--- lib/chess_script.rb
+++ lib/chess_script.rb
@@ -4,7 +4,11 @@ require_relative('user_interface')
 
 include Ui_messages
 
-welcome_message
-testing_board = Chess_board.new
-testing_board.print_board
+# welcome_message
 
+testing_board = Chess_board.new
+test_pawn = Pawn.new
+
+
+testing_board.board[0][0] = test_pawn.name + '-' + test_pawn.color
+p testing_board.board[0] 
